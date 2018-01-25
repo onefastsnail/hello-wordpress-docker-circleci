e4f70cbb1cb2f97a61bdbac5b1573447f9aed25b
Paul Stewart <onefastsnail@gmail.com>
2018-01-25 14:23:24 +0200

    Using pre commit to test, for now existence of files, later things like linting to ensure top grade code

---
+++ test/files.spec.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const assert = require('chai').assert;
+
+describe('Project files', function () {
+
+    it('should have ./.gitignore file', function (done) {
+        checkFileOrDirectory('.gitignore')
+        done()
+    })
+
+    it('should have ./README.md file', function (done) {
+        checkFileOrDirectory('README.md')
+        done()
+    })
+
+    function checkFileOrDirectory(fileOrDirectory) {
+        try {
+        fs.statSync(fileOrDirectory);
+        } catch(e) {
+        assert.isNull(e, 'file or folder doesn\'t exist. '+e);
+        }
+    }
+
+})
