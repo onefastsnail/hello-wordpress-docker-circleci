97bb76c1bb87db80fd95927a6e5b5fc193099293
Paul Stewart <onefastsnail@gmail.com>
2018-01-25 15:55:06 +0200

    To get things moving i have added an .env file on the prod server until i figure out a solution, so just trying to get everything connected then will refactor and improve things

--- deploy.sh
+++ deploy.sh
@@ -12,4 +12,4 @@ echo 'Remove container..'
 docker rm -f hello-circleci-wordpress-docker &> /dev/null
 
 echo 'Run container..'
-docker run -d -p 80:80 --name hello-circleci-wordpress-docker onefastsnail/hello-circleci-wordpress-docker:master
+docker run -d -p 80:80 --env-file /srv/www/hello-circleci-wordpress-docker/.env --name hello-circleci-wordpress-docker onefastsnail/hello-circleci-wordpress-docker:master
