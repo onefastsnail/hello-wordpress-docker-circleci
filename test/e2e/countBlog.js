9955aae4f1be2e6b6f39eef98b0f76dca069b3b1
Paul Stewart <onefastsnail@gmail.com>
2018-01-25 17:53:57 +0200

    Created blog count test

---
+++ test/e2e/countBlog.js
@@ -0,0 +1,29 @@
+const assert = require('assert');
+
+describe('blog items', function () {
+    let page;
+
+    // as we have a browser in the global node object, for this test lets spin up a new page/tab to work with
+    before(async function () {
+        page = await browser.newPage();
+        await page.goto('http://localhost');
+    });
+
+    // after the test close that page/tab
+    after(async function () {
+        await page.close();
+    })
+
+    it('should have paragraphs', async function () {
+
+        // Extract the results from the page
+        const paragraphs = await page.evaluate(() => {
+            return Array.from(document.querySelectorAll('p'));
+        });
+
+        console.log(`${paragraphs.length} blog items found`);
+        expect(paragraphs).to.be.an('array');
+
+    });
+
+});
